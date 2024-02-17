6c1626c24634a8289469cea7171704ec4112665a
OwenLobato <owenlobatov@gmail.com>
2024-02-17 15:09:02 -0600

    Fix app layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,50 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { PrivateRoute, NavBar } from './components/globals';
 import { PublicAppRoutes, AppRoutes } from './routes';
 
-const App = () => {
+const PublicLayout = ({ children }) => {
   return (
-    <Router>
+    <div className='flex flex-col items-center min-h-screen'>{children}</div>
+  );
+};
+
+const AppLayout = ({ children }) => {
+  return (
+    <>
       <NavBar />
       <div className='flex flex-col items-center min-h-screen'>
         <div className='flex justify-center w-11/12 min-h-screen pb-5 pt-16'>
-          <Routes>
-            {/* PUBLIC ROUTES*/}
-            {PublicAppRoutes.map(({ path, component }, index) => (
-              <Route key={index} path={path} element={component} />
-            ))}
-            {/* PRIVATE ROUTES */}
-            {AppRoutes.map(({ path, component }, index) => (
-              <Route
-                key={index}
-                path={path}
-                element={<PrivateRoute>{component}</PrivateRoute>}
-              />
-            ))}
-          </Routes>
+          {children}
         </div>
       </div>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* PUBLIC ROUTES*/}
+        {PublicAppRoutes.map(({ path, component }, index) => (
+          <Route
+            key={index}
+            path={path}
+            element={<PublicLayout>{component}</PublicLayout>}
+          />
+        ))}
+        {/* PRIVATE ROUTES */}
+        {AppRoutes.map(({ path, component }, index) => (
+          <Route
+            key={index}
+            path={path}
+            element={
+              <AppLayout>
+                <PrivateRoute>{component}</PrivateRoute>
+              </AppLayout>
+            }
+          />
+        ))}
+      </Routes>
     </Router>
   );
 };
--- src/components/globals/routing/NotFoundPage.jsx
+++ src/components/globals/routing/NotFoundPage.jsx
@@ -1,40 +1,44 @@
 import { Link } from 'react-router-dom';
+import { NavBar } from '../NavBar';
 
 export const NotFoundPage = () => {
   return (
-    <section className='flex justify-center items-center'>
-      <div className='text-center'>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          strokeWidth='2'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          className='w-24 h-24 text-red-600 mb-8 mx-auto'
-        >
-          <circle cx='12' cy='12' r='10'></circle>
-          <line x1='12' y1='8' x2='12' y2='12'></line>
-          <line x1='12' y1='16' x2='12' y2='16'></line>
-        </svg>
-        <h1 className='text-4xl font-bold mb-4 text-gray-800'>Oops! 🙈</h1>
-        <p className='text-lg text-gray-700 mb-2'>
-          It seems that you've reached a page that doesn't exist.
-        </p>
-        <p className='text-lg text-gray-700 mb-8'>
-          Don't worry, we'll help you find your way back home.
-        </p>
-
-        <div>
-          <Link
-            to='/dashboard'
-            className='inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded transition duration-300 ease-in-out'
+    <>
+      <NavBar />
+      <section className='flex justify-center items-center w-full min-h-screen'>
+        <div className='text-center'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className='w-24 h-24 text-red-600 mb-8 mx-auto'
           >
-            Go home
-          </Link>
+            <circle cx='12' cy='12' r='10'></circle>
+            <line x1='12' y1='8' x2='12' y2='12'></line>
+            <line x1='12' y1='16' x2='12' y2='16'></line>
+          </svg>
+          <h1 className='text-4xl font-bold mb-4 text-gray-800'>Oops! 🙈</h1>
+          <p className='text-lg text-gray-700 mb-2'>
+            It seems that you've reached a page that doesn't exist.
+          </p>
+          <p className='text-lg text-gray-700 mb-8'>
+            Don't worry, we'll help you find your way back home.
+          </p>
+
+          <div>
+            <Link
+              to='/dashboard'
+              className='inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded transition duration-300 ease-in-out'
+            >
+              Go home
+            </Link>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
