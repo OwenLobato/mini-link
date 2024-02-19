834c0e7bd8a2ae881259fcc47fa90d41704127da
OwenLobato <owenlobatov@gmail.com>
2024-02-18 21:28:25 -0600

    Add button variants (contained and outlined)

--- src/components/globals/actions/Button.jsx
+++ src/components/globals/actions/Button.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 
 export const Button = ({
   type = 'button',
+  variant = 'contained',
   icon,
   text,
   onClick = () => {},
@@ -11,14 +12,25 @@ export const Button = ({
 }) => {
   const buttonClass = `${className} flex justify-center items-center py-2 px-4 rounded transition duration-300 ease-in-out`;
 
-  const defaultStyle = 'bg-light-text-main hover:bg-light-btn-hover text-white';
-  const disabledStyle = 'bg-light-text-third text-white cursor-not-allowed';
+  const containedStyle =
+    'bg-light-text-main text-white hover:bg-light-btn-hover';
+  const outlinedStyle =
+    'border border-light-text-main text-light-text-main hover:bg-blue-100';
+
+  const containedDisabledStyle =
+    'bg-light-text-third text-white cursor-not-allowed';
+  const outlinedDisabledStyle =
+    'border border-light-text-third text-light-text-third cursor-not-allowed';
+
+  const buttonStyle = variant === 'contained' ? containedStyle : outlinedStyle;
+  const disabledStyle =
+    variant === 'contained' ? containedDisabledStyle : outlinedDisabledStyle;
 
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`${buttonClass} ${disabled ? disabledStyle : defaultStyle}`}
+      className={`${buttonClass} ${disabled ? disabledStyle : buttonStyle}`}
       disabled={disabled}
       {...restProps}
     >
@@ -36,6 +48,7 @@ export const Button = ({
 
 Button.propTypes = {
   type: PropTypes.oneOf(['button', 'submit', 'reset']),
+  variant: PropTypes.oneOf(['contained', 'outlined']),
   icon: PropTypes.node,
   text: PropTypes.string,
   onClick: PropTypes.func,
