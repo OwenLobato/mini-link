d4f8737044db966b267eb960c1e7e535280ba2f7
OwenLobato <owenlobatov@gmail.com>
2024-02-24 16:24:01 -0600

    Add random url code generator

--- src/components/modules/addresses/SaveLink.jsx
+++ src/components/modules/addresses/SaveLink.jsx
@@ -2,7 +2,10 @@ import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import QRCode from 'react-qr-code';
 import { miniLinkPath } from '../../../helpers/originPaths';
-import { validateForm } from '../../../helpers/formActions';
+import {
+  validateForm,
+  generateRandomString,
+} from '../../../helpers/formActions';
 import useAddresses from '../../../hooks/useAddresses';
 import { Button, Input, Modal } from '../../globals';
 
@@ -60,6 +63,10 @@ export const SaveLink = ({ isEditMode = false }) => {
     }
   };
 
+  const useRandomCode = () => {
+    setLinkData({ ...linkData, urlCode: generateRandomString() });
+  };
+
   const handleCancel = () => {
     navigate('/dashboard');
   };
@@ -96,14 +103,23 @@ export const SaveLink = ({ isEditMode = false }) => {
               value={linkData.name}
               className='w-full'
             />
-            <Input
-              id='urlCode'
-              label='Short URL code'
-              placeholder={'t3sTC0d3'}
-              onChange={handleLinkData}
-              value={linkData.urlCode}
-              className='w-full'
-            />
+            <span className='w-full flex gap-1'>
+              <Input
+                id='urlCode'
+                label='Short URL code'
+                placeholder={'t3sTC0d3'}
+                onChange={handleLinkData}
+                value={linkData.urlCode}
+                className='w-11/12'
+              />
+              <div
+                title='Generate random URL Code'
+                onClick={useRandomCode}
+                className='flex justify-center items-center w-1/12 rounded-full hover:cursor-pointer hover:bg-slate-50'
+              >
+                <i className='fa-solid fa-wand-magic-sparkles text-light-text-main' />
+              </div>
+            </span>
             <Input
               id='originalLink'
               label='Original URL'
